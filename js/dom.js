86c1bdb61c13ba16bd9a9521056a7637c2a2009b
Boombag0607 <ananyagautam0607@gmail.com>
2021-04-15 21:12:35 +0530

    collapse on click

--- js/dom.js
+++ js/dom.js
@@ -22,3 +22,11 @@ $(document).ready(function(){
       } // End if
     });
   });
+ 
+ 
+  $(function(){ 
+    var navMain = $("#navbarSupportedContent");
+    navMain.on("click", "a", null, function () {
+        navMain.collapse('hide');
+    });
+});
